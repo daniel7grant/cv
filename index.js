d55cd4158341e8ff33e2d643c26223433cd71a15
Grant <gergely.daniel7@gmail.com>
2018-05-23 18:17:31 -0400

    Deploy on server, move frontend files to public/

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+var PORT = process.env.PORT || 8082;
+
+app.use(express.static(path.join(__dirname, 'public/dist')));
+app.use(express.static(path.join(__dirname, 'public/static')));
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, 'public/dist/index.html'));
+});
+
+app.get('/hu', function(req, res){
+	res.sendFile(path.join(__dirname, 'public/dist/index.html'));
+});
+
+app.get('/en', function(req, res){
+	res.sendFile(path.join(__dirname, 'public/dist/index.html'));
+});
+
+app.listen(PORT, function(){
+	console.log('cv.danielgrants.com is running on port ' + PORT);
+});
