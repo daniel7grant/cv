6b23461b29ec9c87a82836db1938303126c7ac2b
Grant <gergely.daniel7@gmail.com>
2018-06-23 00:05:24 +0200

    Try to test out the 90-line js file (unsuccessfully)

--- public/test/index.test.js
+++ public/test/index.test.js
@@ -1,4 +1,10 @@
-import { positionLangs, activateElements, toggleElement, deactivateElements } from '../src/index.js'
+import { positionLangs, activateElements, toggleElement, deactivateElements, calculateRadius } from '../src/index.js';
+import DoSlide from 'do-slide';
+import Clipboard from 'clipboard';
+import ga from '../src/analytics.js'
+jest.mock('do-slide');
+jest.mock('clipboard');
+jest.mock('../src/analytics.js');
 
 describe('positioning elements circularly', () => {
 	test('all elements gain a css transform', () => {
@@ -64,7 +70,7 @@ describe('positioning elements circularly', () => {
 	});
 });
 
-describe('activating elements', () => {
+describe('elements activation', () => {
 	test('adds class "is-active" to one element', () => {
 		let mockElements = document.createElement('div');
 
@@ -84,7 +90,7 @@ describe('activating elements', () => {
 	});
 });
 
-describe('toggling one element\'s "is-active" class', () => {
+describe('one element\'s "is-active" class toggling', () => {
 	test('adds class "is-active" to one element', () => {
 		let mockEvent = { stopPropagation: () => { } };
 		let mockElement = document.createElement('div');
@@ -121,7 +127,7 @@ describe('toggling one element\'s "is-active" class', () => {
 
 });
 
-describe('deactivating elements', () => {
+describe('element deactivation', () => {
 	test('adds class "is-active" to one element', () => {
 		let mockEvent = { stopPropagation: () => { } };
 		let mockElement = document.createElement('div');
@@ -149,5 +155,120 @@ describe('deactivating elements', () => {
 		let mockElement = document.createElement('div');
 
 		toggleElement(mockEvent, mockElement);
+	});
+});
+
+describe('radius calculation', () => {
+
+	beforeAll(() => {
+		Object.defineProperty(window, 'matchMedia', {
+			value: jest.fn((q) => { return { matches: true } })
+		});
+
+		Object.defineProperty(window, 'innerWidth', { value: 600 });
+		Object.defineProperty(window, 'innerHeight', { value: 500 });
+	});
+
+	test('maxes with 300 if window larger than 800x800', () => {
+		//global.window.matchMedia = jest.fn(() => { matches: true });
+
+		expect(calculateRadius()).toBe(300);
+	});
+
+	test('returns the 80% of the half of the smaller dimension', () => {
+		window.matchMedia.mockReturnValueOnce({ matches: false });
+
+		expect(calculateRadius()).toBeCloseTo(200);
+	});
+});
+
+describe('DOMContentLoaded event', () => {
+	const addEventListener = jest.fn(function (event) { return {className: this.className, event: event} });
+
+	beforeAll(() => {
+		Object.defineProperty(window, 'matchMedia', {
+			value: jest.fn((q) => { return { matches: true } })
+		});
+
+		Object.defineProperty(document, 'getElementsByClassName', {
+			value: (name) => {
+				return [{
+					className: name,
+					style: {},
+					addEventListener: addEventListener
+				}];
+			}
+		});
+
+		Object.defineProperty(document, 'querySelectorAll', {
+			value: (name) => {
+				return [{
+					className: name,
+					style: {},
+					addEventListener: addEventListener
+				}];
+			}
+		});
+	});
+
+	beforeEach(() => {
+		DoSlide.mockClear();
+		Clipboard.mockClear();
+	});
+
+	test('creates DoSlide object for slides', () => {
+		window.document.dispatchEvent(new Event('DOMContentLoaded'));
+
+		expect(DoSlide).toHaveBeenCalledWith('#slide-container', { activeClass: 'current-slide' });
+	});
+
+	test('creates DoSlide object for projects if media is large', () => {
+		window.document.dispatchEvent(new Event('DOMContentLoaded'));
+
+		expect(DoSlide).toHaveBeenCalledWith('#slide-projects-list', {
+			horizontal: true,
+			listenUserMouseWheel: false
+		});
+	});
+
+	test('does not create DoSlide object for projects if media is small', () => {
+		window.matchMedia.mockReturnValueOnce({ matches: false });
+
+		window.document.dispatchEvent(new Event('DOMContentLoaded'));
+
+		expect(DoSlide).not.toHaveBeenCalledWith('#slide-projects-list', {
+			horizontal: true,
+			listenUserMouseWheel: false
+		});
+	});
+
+	test('creates Clipboard object', () => {
+		window.document.dispatchEvent(new Event('DOMContentLoaded'));
+		expect(Clipboard).toHaveBeenCalledWith('a.copy');
+	});
+
+	test('adds click event to class project-cards', () => {
+		window.document.dispatchEvent(new Event('DOMContentLoaded'));
+		expect(addEventListener).toHaveReturnedWith({ className: 'project-card', event: 'click' });
+	});
+
+	test('adds click event to class close', () => {
+		window.document.dispatchEvent(new Event('DOMContentLoaded'));
+		expect(addEventListener).toHaveReturnedWith({ className: 'close', event: 'click' });
+	});
+
+	test('adds click event to query input.contact-input', () => {
+		window.document.dispatchEvent(new Event('DOMContentLoaded'));
+		expect(addEventListener).toHaveReturnedWith({ className: 'input.contact-input', event: 'click' });
+	});
+
+	test('adds click event to query a.copy', () => {
+		window.document.dispatchEvent(new Event('DOMContentLoaded'));
+		expect(addEventListener).toHaveReturnedWith({ className: 'a.copy', event: 'click' });
+	});
+
+	test('adds click event to query img.arrow', () => {
+		window.document.dispatchEvent(new Event('DOMContentLoaded'));
+		expect(addEventListener).toHaveReturnedWith({ className: 'img.arrow', event: 'click' });
 	});
 });
