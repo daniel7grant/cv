55450791fd9633697b3172ec10e788330538a1ba
Grant <gergely.daniel7@gmail.com>
2023-01-23 22:27:30 +0100

    Fix webpack dev server

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -26,21 +26,28 @@ module.exports = {
 	plugins: [
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
-			filename: 'hu.html',
-			minify: true,
-			title: 'Gergely Dániel',
-			template: './src/templates/index.hbs',
-			templateParameters: content.hu
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'en.html',
+			filename: 'index.html',
 			minify: true,
 			title: 'Gergely Dániel',
 			template: './src/templates/index.hbs',
 			templateParameters: content.en
 		}),
 		new HtmlWebpackPlugin({
-			filename: '404.html',
+			filename: 'hu/index.html',
+			minify: true,
+			title: 'Gergely Dániel',
+			template: './src/templates/index.hbs',
+			templateParameters: content.hu
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'en/index.html',
+			minify: true,
+			title: 'Gergely Dániel',
+			template: './src/templates/index.hbs',
+			templateParameters: content.en
+		}),
+		new HtmlWebpackPlugin({
+			filename: '404/index.html',
 			minify: true,
 			title: 'Gergely Dániel',
 			template: './src/templates/404.hbs',
@@ -49,5 +56,11 @@ module.exports = {
 		new MiniCssExtractPlugin({
 			filename: 'theme.[chunkhash].css'
 		})
-	]
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'static'),
+		},
+		compress: true,
+	},
 };
