76dbe2eb316dc08d78ddabd1c92d2f71a82d7421
Grant <gergely.daniel7@gmail.com>
2023-08-05 22:35:23 +0200

    Add gr to the list of projects

--- public/src/content.js
+++ public/src/content.js
@@ -515,6 +515,38 @@ module.exports = {
                 ],
             },
             {
+                name: 'gr',
+                cover: { png: 'gr.png', webp: 'gr.webp' },
+                url: 'https://github.com/daniel7grant/gr',
+                client: {
+                    name: 'Open Source',
+                    url: 'https://github.com/daniel7grant',
+                },
+                details: [
+                    'Rust-based command-line interface to interact with git servers',
+                    'Compatible with GitHub, GitLab, Bitbucket and Gitea',
+                    'Install with Cargo or NPM',
+                ],
+                languages: [
+                    {
+                        name: 'Rust',
+                        icon: '/icons/rust/rust-plain.svg',
+                    },
+                    {
+                        name: 'NPM',
+                        icon: '/icons/npm/npm-original-wordmark.svg',
+                    },
+                    {
+                        name: 'GitHub',
+                        icon: '/icons/github/github-original.svg',
+                    },
+                    {
+                        name: 'GitLab',
+                        icon: '/icons/gitlab/gitlab-original.svg',
+                    },
+                ],
+            },
+            {
                 name: 'KTK infrastructure',
                 cover: { png: 'argo.png', webp: 'argo.webp' },
                 url: '',
