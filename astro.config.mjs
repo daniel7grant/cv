0fda5b71998812d98c9f8a3306a796ad998c9b2f
Grant <gergely.daniel7@gmail.com>
2026-02-09 23:25:38 +0100

    Add site path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+    site: 'https://cv.danielgrants.com',
+    integrations: [svelte()],
 });
